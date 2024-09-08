d006cc49590ee3b9e2c492eef6c61dc99feb26a4
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-09-08 20:02:33 +0100

    Feat: Add route for change password

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthLoginView from '@/views/auth/AuthLoginView.vue'
 import HomePageView from '@/views/HomePageView.vue'
+import ChangePasswordView from '@/views/auth/ChangePasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'login', component: AuthLoginView },
-    { path: '/home', name: 'home', component: HomePageView }
+    { path: '/home', name: 'home', component: HomePageView },
+    { path: '/changePassword', name: 'changePassword', component: ChangePasswordView }
   ]
 })
 
