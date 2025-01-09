e72e6a634afb14a7567dd0e8a41a9a8584093f08
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-01-09 14:52:12 +0100

    Refactor: Add responsive design for my navbar

--- src/components/navbar/NavBarLogin.vue
+++ src/components/navbar/NavBarLogin.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+const  isMenuOpen= ref(false)
+const toggleMenu=() =>{
+    isMenuOpen.value = !isMenuOpen.value;
+}
 
 const name = 'Nav'
 </script>
@@ -20,17 +24,26 @@ const name = 'Nav'
             </ul>
           </div>
         </div>
-        <div
-          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-        >
-          <p class="text-white">Connexion</p>
+        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+          <button @click="toggleMenu" class="text-white focus:outline-none">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
         </div>
-        <div
-          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-        >
-          <router-link class="flex items-center justify-between text-white" to="/register">
-            Inscription
-          </router-link>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <ul :class="{ 'hidden': !isMenuOpen, 'block': isMenuOpen }" class="absolute inset-x-0 top-full bg-cyan-500 py-4 px-2 sm:static sm:flex sm:flex-row sm:bg-transparent sm:space-x-5">
+            <li class="text-lg">
+              <router-link class="flex items-center justify-between text-white" to="/">
+                Connexion
+              </router-link>
+            </li>
+            <li class="text-lg">
+              <router-link class="flex items-center justify-between text-white" to="/register">
+                Inscription
+              </router-link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
