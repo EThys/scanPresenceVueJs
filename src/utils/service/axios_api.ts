066b5f3e457f6a8100c7fa04865c416d98679418
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-01-10 10:56:15 +0100

    Refactor: change endpoint for prod url

--- src/utils/service/axios_api.ts
+++ src/utils/service/axios_api.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from 'axios'
 
 export const useAxiosRequestWithToken = (token: string = ''): AxiosInstance => {
   const useAxios: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://213.136.74.84:703/api/',
     headers: {
       accept: 'application/json',
       'Content-type': 'application/json',
@@ -16,7 +16,7 @@ export const useAxiosRequestWithToken = (token: string = ''): AxiosInstance => {
 
 export const useAxiosRequestWithTokenForCsv = (token: string = ''): AxiosInstance => {
   const useAxios: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://213.136.74.84:703/api/',
     headers: {
       accept: 'application/json',
       'Content-Type': 'multipart/form-data',
